d242e8d9a04ea0bbe045560dbfe30ed6d45c460f
ning <ningyuan.sg@gmail.com>
2018-03-05 22:44:26 +0800

    Add calculation for coefficient of variation

--- 03-Density.r
+++ 03-Density.r
@@ -19,7 +19,9 @@ myna_mean <- mean(myna$Density)
 myna_var  <- var(myna$Density)
 paste('  mean: ', myna_mean, ', var: ', myna_var, sep='')
 myna_ci <- qt(0.9750, df=n-1) * sqrt(myna_var) / sqrt(n)
-paste('  err: ', myna_ci, sep='')
+paste('  err: ', myna_ci, 
+      ', %CV: ', (sqrt(myna_var)/myna_mean)*100,
+    sep='')
 paste('  total pop: ', myna_mean * 1.5, ' +- ', myna_ci * 1.5, sep='')
 
 print('Yellow-vented bulbul')
@@ -27,31 +29,39 @@ bulbul_mean <- mean(bulbul$Density)
 bulbul_var  <- var(bulbul$Density)
 paste('  mean: ', bulbul_mean, ', var: ', bulbul_var, sep='')
 bulbul_ci <- qt(0.9750, df=n-1) * sqrt(bulbul_var) / sqrt(n)
-paste('  err: ', bulbul_ci, sep='')
+paste('  err: ', bulbul_ci, 
+      ', %cv: ', (sqrt(bulbul_var)/bulbul_mean)*100,
+    sep='')
 paste('  total pop: ', bulbul_mean * 1.5, ' +- ', bulbul_ci * 1.5, sep='')
-
-print('Rock pigeon')
-pigeon_mean <- mean(pigeon$Density)
-pigeon_var  <- var(pigeon$Density)
-paste('  mean: ', pigeon_mean, ', var: ', pigeon_var, sep='')
-pigeon_ci <- qt(0.9750, df=n-1) * sqrt(pigeon_var) / sqrt(n)
-paste('  err: ', pigeon_ci, sep='')
-paste('  total pop: ', pigeon_mean * 1.5, ' +- ', pigeon_ci * 1.5, sep='')
 
 print('Olive-backed sunbird')
 sunbird_mean <- mean(sunbird$Density)
 sunbird_var  <- var(sunbird$Density)
 paste('  mean: ', sunbird_mean, ', var: ', sunbird_var, sep='')
 sunbird_ci <- qt(0.9750, df=n-1) * sqrt(sunbird_var) / sqrt(n)
-paste('  err: ', sunbird_ci, sep='')
+paste('  err: ', sunbird_ci, 
+      ', %cv: ', (sqrt(sunbird_var)/sunbird_mean)*100,
+    sep='')
 paste('  total pop: ', sunbird_mean * 1.5, ' +- ', sunbird_ci * 1.5, sep='')
+
+print('Rock pigeon')
+pigeon_mean <- mean(pigeon$Density)
+pigeon_var  <- var(pigeon$Density)
+paste('  mean: ', pigeon_mean, ', var: ', pigeon_var, sep='')
+pigeon_ci <- qt(0.9750, df=n-1) * sqrt(pigeon_var) / sqrt(n)
+paste('  err: ', pigeon_ci, 
+      ', %CV: ', (sqrt(pigeon_var)/pigeon_mean)*100,
+    sep='')
+paste('  total pop: ', pigeon_mean * 1.5, ' +- ', pigeon_ci * 1.5, sep='')
 
 print('Black-naped oriole')
 oriole_mean <- mean(oriole$Density)
 oriole_var  <- var(oriole$Density)
 paste('  mean: ', oriole_mean, ', var: ', oriole_var, sep='')
 oriole_ci <- qt(0.9750, df=n-1) * sqrt(oriole_var) / sqrt(n)
-paste('  err: ', oriole_ci, sep='')
+paste('  err: ', oriole_ci, 
+          ', %CV: ', (sqrt(oriole_var)/oriole_mean)*100,
+    sep='')
 paste('  total pop: ', oriole_mean * 1.5, ' +- ', oriole_ci * 1.5, sep='')
 
 gm <- ggplot(myna, aes(x='Javan myna', y=Density)) +
