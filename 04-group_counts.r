edcfe379d34d7c2db41619cecb231d3240038687
ning <ningyuan.sg@gmail.com>
2018-03-10 19:02:44 +0800

    Omit birds not of 5 selected species

--- 04-group_counts.r
+++ 04-group_counts.r
@@ -13,14 +13,27 @@ get_cv <- function(df) {
     sd/avg * 100
 }
 
+filter_5 <- function(df) {
+    # @returns only observations of 5 species
+    df[
+        df$Bird.Species == 'Javan myna' |
+        df$Bird.Species == 'Yellow-vented bulbul' |
+        df$Bird.Species == 'Olive-backed sunbird' |
+        df$Bird.Species == 'Rock pigeon' |
+        df$Bird.Species == 'Black-naped oriole'
+    , ]
+}
+
 df <- read.csv('data.csv')
 df <- df[!is.na(df$Bird.Species), ]  # only confident IDs
 df <- df[!is.na(df$Distance.Bin), ]  # no flybys or heard-not-seens
+df <- filter_5(df)
 obs_per_group <- count(df$Group.Name)
 names(obs_per_group) <- c('Group Name', 'Observations')
 obs_per_group <- data.frame(obs_per_group)
 
 print(obs_per_group)  # tabular print
+cat('Total:', sum(obs_per_group$Observations), fill=TRUE)
 
 cv <- get_cv(obs_per_group$Observations)
 cv <- round(cv, digits=3)
@@ -31,6 +44,6 @@ cat(cv_text, fill=TRUE)
 g <- ggplot(obs_per_group, aes(x='', y=Observations)) +
     geom_boxplot() + coord_flip() +  # horiziontal boxplot
     labs(x='') +
-    annotate('text', label=cv_text, x=0.6, y=172)  
+    annotate('text', label=cv_text, x=0.6, y=152)  
 
 ggsave('04-group_counts.png', width=6, height=2)
